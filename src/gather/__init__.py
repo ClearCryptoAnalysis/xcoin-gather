6c666215d6421e07adeafc555c396884ead291e2
William Adams <wil.h.adams@gmail.com>
2022-08-01 10:27:55 -0400

    Add SOL coin

--- src/gather/__init__.py
+++ src/gather/__init__.py
@@ -3,3 +3,4 @@
 from .binancesmartchain import GatherDataBinanceSmartChain
 from .bitcoin import GatherDataBitcoin
 from .ethereum import GatherDataEthereum
+from .solana import GatherDataSolana
---
+++ src/gather/solana.py
@@ -0,0 +1,39 @@
+from json import JSONDecodeError
+import requests
+
+from .base import GatherDataBase
+
+
+class GatherDataSolana(GatherDataBase):
+    def __init__(self, base_dir,):
+        super().__init__(
+            name="Solana",
+            abbreviation="SOL",
+            base_dir=base_dir,
+            data_chunk_properties={
+                "chunk_size": 100000,
+                "zfill_len": 4,
+            }
+        )
+
+    @staticmethod
+    def get_block_by_number(block_number, session=requests.Session()):
+        headers = {"Content-Type": "application/json",}
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "getBlock",
+            "params": [block_number,],
+            "id": 69420,
+        }
+        request = session.post(
+            "https://rpc.ankr.com/solana",
+            json=payload,
+            headers=headers,
+            proxies={
+                "http": "socks5://149.56.96.252:9300"
+            },
+        )
+        try:
+            return request.json()["result"]
+        except JSONDecodeError:
+            return "\n"
